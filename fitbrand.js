1ec055aba5eab999538df3a3671bc2ef7e9b8c1f
FitBrand-co <adrianemilontop@gmail.com>
2026-04-27 09:49:36 +0200

    Update fitbrand.js

--- fitbrand.js
+++ fitbrand.js
@@ -28,3 +28,12 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+window.addEventListener("scroll", function(){
+  const nav = document.querySelector(".nav");
+
+  if(window.scrollY > 40){
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+});
